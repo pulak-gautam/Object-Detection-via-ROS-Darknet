7cdd23b87f1676e30bafe77ce53a9aeaa5bb6a73
atulyasndrm <aatulya21@iitk.ac.in>
2023-06-20 22:31:06 +0530

    Completed Sync

--- launch.sh
+++ launch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-gnome-terminal --tab -- bash -c "gazebo --verbose ~/Projects/Aerial/ardupilot_gazebo/worlds/iris_arducopter_runway.world"
+gnome-terminal --tab -- bash -c "gazebo --verbose /home/atulya/Workspaces/robofest23/src/Object-Detection-via-ROS-Darknet/communication/worlds/iris_arducopter_runway.world"
+
+echo "Press ENTER once Gazebo launched"
+read $1
+
 gnome-terminal --tab -- bash -c "sim_vehicle.py -v ArduCopter -f gazebo-drone1 -I1 --out=tcpin:0.0.0.0:8100 " 
 gnome-terminal --tab -- bash -c "sim_vehicle.py -v ArduCopter -f gazebo-drone2 -I2 --out=tcpin:0.0.0.0:8200 " 
 gnome-terminal --tab -- bash -c "sim_vehicle.py -v ArduCopter -f gazebo-drone3 -I3 --out=tcpin:0.0.0.0:8300 "
